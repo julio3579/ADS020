b51b1022c624dc6159039a9267b3b3423bb5378b
16114290038 <16114290038@OLAB04-D37.academico.iesb.br>
2020-02-11 23:07:25 -0200

    Atividade01

---
+++ Atividades/Atividade01/src/modelo/entidades/aluno.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.entidades;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author 16114290038
+ */
+public class aluno implements Serializable {
+    private int id;
+    private int matricula;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
