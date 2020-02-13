84cabe692f8a52f28a91cafa21c8f585186296de
16114290038 <16114290038@OLAB04-D37.academico.iesb.br>
2020-02-12 22:55:58 -0200

    Atividade01

---
+++ Atividades/Atividade01/src/modelo/BO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.List;
+
+/**
+ *
+ * @author 16114290038
+ */
+public interface BO <T>{
+    
+    List <T> listar() throws NegocioException;
+    void incluir(T entidade) throws NegocioException;
+    void alterar(T entidade) throws NegocioException;
+    void excluir(T entidade) throws NegocioException;
+    void validar(T entidade) throws NegocioException;
+    T consultar(T entidade) throws NegocioException;
+}
+
